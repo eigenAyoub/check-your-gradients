db8d2cabee005b13fd55454db995b917c1ec9b69
a17benayad <a17benayad@gmail.com>
2022-12-06 22:30:10 +0100

    first working demo NN from scratch

---
+++ README.md
@@ -0,0 +1,39 @@
+I'm starting a backprop project. Expect to see a lot of Gradients, Hessians, JAX and PyTorch.
+
+See you!
+
+I'll be working daily on this project.
+ 
+### Abstract:
+My main goal is to learn Pytorch/Jax, but also have a clear vision on what's happening underneath. I intend to build and train NNs from scratch, but have a PyTorch/Jax implelemention in parallel for correctness. Similar to what Andrej Karpathy have done in [Micrograd](https://github.com/karpathy/micrograd) with Pytorch.
+
+
+### Goals:
+
+[1] Be able to build and train NNs from scratch.
+[2] Focus on Modularity, and Optimization of code.
+[3] Learn & check correctness with PyTorch/Jax.
+[4] See how it is done PyTorch/Jax on a deeper level.
+
+
+### Steps:
+[1] Build a starting NN:
+    [1] Set up a working env:
+        * Load MNIST
+    [2] Train a simple MLP to classify MNIST
+        * Forward.
+        * Backward.
+        * Loop and make sure the NN is learning.
+    [DONE] Current accuracy with a simple MLP is 0.934.
+    [3] Learn how to load the data faster.
+    [4] Try a softmax layer
+    [5] Vertorize the tensor manipulations (the softmax layer)
+[2] Make it modular.
+[3] Start the project.
+
+
+### References:
+[1] Andrej Karpathy
+[2] Grokking DL
+[3] PyTorch / Jax
+
---
+++ main.py
@@ -0,0 +1,184 @@
+import matplotlib.pyplot as plt
+from sklearn import datasets
+
+import numpy as np
+
+from numpy.random import normal
+
+# data loading
+dataset = datasets.load_digits()
+data = dataset.data
+target = dataset.target
+
+# Constructing 1 hot encoding of the labels (I'm not sure if this what they called)
+y = np.zeros((target.shape[0],10))
+y[range(target.shape[0]),target] = 1
+
+# train / test split
+X1, X2 = data[:1600], data[1600:]
+Y1, Y2 = y[:1600],y[1600:]
+
+
+y_test = target[1600:]
+
+batch = 16
+
+# shapes >> X (N,64)
+#        >> Y (N,)
+
+
+# Layers:
+
+# Xavier Initia (Does any one know why it works? Poke: ML Alchemy)
+
+W1 = normal(scale = 2/64,  size=(64,70))
+W2 = normal(scale = 2/70,  size=(70,300))
+W3 = normal(scale = 2/300, size=(300,10))
+
+b1 = np.zeros((70,))
+b2 = np.zeros((300,))
+b3 = np.zeros((10,))
+
+
+# Computing the LOSS:
+
+def quadraticLoss(pred, labels):
+    """
+    Compute the quadratic loss:
+    L = \sum (y_i - x_i)^2
+    """
+
+    l = (pred -  labels)**2
+    return np.sum(l)
+
+
+
+# Computing the softmax layer:
+def softmax(l):
+    """
+    compute a vectorized softmax layer
+
+    """
+    ll = np.exp(l - l.max(axis=1, keepdims=True))
+    return ll / np.sum(ll, axis=1, keepdims=True)
+
+
+
+def forward(X):
+    Z1 = X@W1 + b1
+    L1 = Z1*(Z1>0)
+    Z2 = L1@W2 + b2
+    L2 = Z2*(Z2>0)
+    Z3 = L2@W3 + b3
+    return 1/(1+np.exp(-Z3))
+
+
+epoch = 500
+batches = 100
+
+acc = []
+
+for _ in range(epoch): 
+    for k in range(batches):
+
+        X = X1[k*batch: (k+1)*batch]
+        yy = Y1[k*batch: (k+1)*batch]
+        labs = target[k*batch:(k+1)*batch]
+        
+        # forward pass
+        Z1 = X@W1 + b1
+        L1 = Z1*(Z1>0)
+        Z2 = L1@W2 + b2
+        L2 = Z2*(Z2>0)
+        Z3 = L2@W3 + b3
+        L3 = 1/(1+np.exp(-Z3))
+
+        diff = (L3-yy)**2
+
+        loss = np.sum(diff)
+
+        # print(loss)
+
+        #######
+        #######
+
+        dL3 = 2*(L3-yy)
+        
+        dZ3 = L3*(1-L3)*dL3
+
+        dW3 = L2.T @ dZ3
+        db3 = np.sum(dZ3, axis=0) 
+        dL2 = dZ3 @ W3.T 
+
+        dZ2 = dL2 * (Z2>0)
+
+        dW2 = L1.T @ dZ2 
+        db2 = np.sum(dZ2, axis=0)
+        dL1 = dZ2 @ W2.T 
+
+        dZ1 = dL1 * (Z1>0) 
+
+        dW1 = X.T @ dZ1
+        db1 = np.sum(dZ1, axis=0)
+          
+        # updates:
+        
+        alpha = 0.0001
+
+        W3 -= alpha * dW3
+        W2 -= alpha * dW2
+        W1 -= alpha * dW1 
+
+        b1 -= alpha * db1
+        b2 -= alpha * db2 
+        b3 -= alpha * db3
+
+
+    test = forward(X2)
+    equal= np.argmax(test, axis=1) == y_test
+
+    acc.append(np.sum(equal)/len(equal))
+
+
+
+
+"""
+        L3 = Z3*(Z3>0)
+
+        L4 = softmax(L3) 
+#        print(L4[0])
+#        print(L4[range(batch),labs])
+        L = -np.log(L4[range(batch),labs]+0.01)
+        
+#        loss = quadraticLoss(L4,yy)
+#        cross= cross_entropy(L4, labs)
+
+        print(np.sum(L))
+
+#        print(loss)
+
+        # backward pass:
+        dL4 = np.copy(yy)  
+
+        dL4[range(batch), labs] = -1/L4[range(batch),labs]
+
+        dL4_ = 2*yy*(yy-L4)
+
+        dL4_dL3 = np.zeros((batch, 10, 10))
+        for i in range(batch):
+            for j in range(10):
+                dL4_dL3[i,j,:] = np.array([-L4[i, j]*L4[i, k] for k in range(10)])
+                dL4_dL3[i,j,j] = L4[i,j]*(1-L4[i,j])
+                
+        dL3 = np.zeros((batch, 10))
+        for m in range(batch):
+            for n in range(10):
+                dL3[m,n] = dL4[m] @ dL4_dL3[m,n]
+
+        #for j in range(10):
+        #    dL3[:,j] = np.diag(L4 @ dL4_dL3[:,j,:].T) 
+
+#        dL3_dZ3 = 1 * (Z3>0)               
+#        dZ3 = dL3 * dL3_dZ3
+"""
+
