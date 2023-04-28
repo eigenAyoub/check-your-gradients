fixed the loading, added softmax layer, and cross entropy later

MNISTDataset.py
@@ -1,10 +1,12 @@
+import torch
 from torch.utils.data import Dataset
 
 class MNISTDataset(Dataset):
     'Characterizes a dataset for PyTorch'                                       
-    def __init__(self, data, N):                                                
+    def __init__(self, X, Y, N):                                                
         'Initialization'                                                        
-        self.data = data                                                        
+        self.data = X 
+        self.target = Y 
         self.list_IDs = range(N)                                                
     def __len__(self):                                                          
         'Denotes the total number of samples'                                   
@@ -12,10 +14,9 @@ class MNISTDataset(Dataset):
                                                                                 
     def __getitem__(self, index):                                               
         'Generates one sample of data'                                          
-        # Select sample                                                         
-        ID = self.list_IDs[index]                                               
-        X = self.data.data[10]                                                  
-        y = self.data.target[10]                                                
-                                                                                
-        return X, y
+        x = self.data[index]                                                  
+        y = torch.zeros(10)
+        y[self.target[index]] = 1
+
+        return x, y
 

Tensor.py
@@ -8,6 +8,9 @@ from torch.utils.data import Dataset, DataLoader
 
 from MNISTDataset import MNISTDataset
 
+import torch
+
+
 class Tensor(object):
     """Docstring for Tensor. """
 
@@ -22,7 +25,6 @@ class Tensor(object):
 
         # Gradients >> Now everyone can get it...
         self.grad = np.zeros(data.shape)
-        
 
     def __repr__(self):
         return str(self.data.__repr__())
@@ -30,12 +32,12 @@ class Tensor(object):
     def __str__(self):
         return str(self.data.__str__())
 
-    # Operations:
 
-    # Magic functions:
+    # Operations / Magic functions:
 
     def __sub__(self, other):
-        output = Tensor(self.data - other.data, label=self.label+"-"+other.label)
+        output = Tensor(self.data - other.data,
+                        label=self.label+"-"+other.label)
         output.roots = [self, other]
         output.how = "-"
         
@@ -57,27 +59,41 @@ class Tensor(object):
 
         return  output 
 
+    def softmax_layer(self):
+        mx = torch.argmax(self.data, dim = 1)[:,None]
+        sm = torch.sum(self.data, dim = 1)[:,None] 
+        output = torch.exp(self.data - mx)/sm
+        output = Tensor(output.numpy())
 
-    def entropy(self, other):
-        ypred = self.data
-        yhat = other.data
-
-        output = -np.log(yhat(
-
-        output = Tensor(np.sum(self.data**2), label="l")
-        output.grad = np.ones(self.data.shape)
-        output.roots = [self]
-        output.how = "Sum"
-        
         def pass_the_gradient():
             """this function does:
             > Updates the gradients of the parents/roots.
             """
-            self.grad += 2*self.data
+            self.grad += self.output * (output.grad - 
+                                        torch.sum(G * output.data, 
+                                                  dim=1)[:, None]
+                                        )
 
         output.pass_the_grad = pass_the_gradient
 
         return  output 
+
+    def cross_entropy_loss(self, other):
+        """Assumes a softmax layer as input"""
+        output = -np.sum(other.data.numpy() * np.log(self.data)
+                            , axis=1 )
+        output = Tensor(output)
+
+        def pass_the_gradient():
+            """this function does:
+            > Updates the gradients of the parents/roots.
+            """
+            self.grad += - self.other / self.data
+
+        output.pass_the_grad = pass_the_gradient
+
+        return  output 
+
 
     def quadratic_loss(self):
         output = Tensor(np.sum(self.data**2), label="l")
@@ -96,7 +112,8 @@ class Tensor(object):
         return  output 
 
     def __add__(self, other):
-        output = Tensor(self.data + other.data, label=self.label+"+"+other.label)
+        output = Tensor(self.data + other.data, 
+                        label=self.label+"+"+other.label)
         output.roots = [self, other]
         output.how = "+"
         
@@ -119,7 +136,9 @@ class Tensor(object):
         return  output 
 
     def __mul__(self, other):
-        output = Tensor(self.data * other.data, label=self.label+"*"+other.label)
+
+        output = Tensor(self.data * other.data, 
+                        label=self.label+"*"+other.label)
         output.roots = [self, other] 
         output.how = "*"
         
@@ -160,7 +179,9 @@ class Tensor(object):
             self.grad += output.data*(1-output.data) * output.grad
         output.pass_the_grad = pass_the_gradient
 
-        return output 
+        return output
+
+
 
     def comp_graph(self):
         gr = []

main.py
@@ -2,14 +2,13 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 
 import numpy as np
-
 from numpy.random import normal, randint
-
 
 from Tensor import Tensor
 from MNISTDataset import MNISTDataset
 
-
+import torch
+from torch.utils.data import DataLoader
 
 # weights:
 w1_ = normal(scale = 1/3 ,size=(64, 120))                                       
@@ -29,13 +28,11 @@ batch_size = 20
 
 # Loading using DataSet and DataLoader:
 digits = datasets.load_digits()
-g = MNISTDataset(digits, N)
+g = MNISTDataset(digits.data, digits.target, N)
 data_loader = DataLoader(g, batch_size=16, shuffle=True)
 
-
-for i in range(0) :                                                             
-    x_, y_ =  next(iter(data_loader))                                           
-    x_, y_ = Tensor(x_), Tensor(y_)                                             
+x_, y_ =  next(iter(data_loader))                                           
+x, y = Tensor(x_), Tensor(y_)                                             
                                                                                 
 alpha = 0.01                                                                    
 
@@ -45,17 +42,24 @@ def forward(d):
 
 
 
-def pass_(x, y):
-    Z1 = x@W1; Z1.label = "Z1"
-    Z12 = Z1 + b1; Z12.label = "Z12"
-    L1 = Z12.sigmoid()
-    Z2 = L1@W2; Z2.label="Z2"
-    Z22 = Z2 + b2; Z22.label = "Z22" 
-    L2 = Z22.sigmoid()
-    print(L2.data.size(), y.data.size())
-    L3 = L2-y
-    loss = L3.quadratic_loss()
-    
+#def pass_(x, y):
+Z1 = x@W1; Z1.label = "Z1"
+Z12 = Z1 + b1; Z12.label = "Z12"
+L1 = Z12.sigmoid()
+Z2 = L1@W2; Z2.label = "Z2"
+Z22 = Z2 + b2; Z22.label = "Z22" 
+L2 = Z22.sigmoid()
+#print(L2.data.size(), y.data.size())
+Lsoft = L2.softmax_layer()
+
+
+
+
+for i in range(0):
+    # forward pass:
+
+
+    # backward pass:
     loss.pass_the_grad()
     L3.pass_the_grad()
     L2.pass_the_grad()
@@ -87,6 +91,3 @@ def pass_(x, y):
     b1.grad =  np.zeros(b1.grad.shape)
     W2.grad =  np.zeros(W2.grad.shape)
     b2.grad =  np.zeros(b2.grad.shape)
-
-
-                          